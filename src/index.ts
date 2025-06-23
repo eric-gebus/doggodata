6074e31a0b5fac29d3c24ddd973cc583f787db88
eric-gebus <ericgebus@gmail.com>
2025-06-23 17:58:07 +0100

    Fix routing and exports

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,14 @@ const app = express();
 app.use(express.json());
 app.use(cors())
 
-app.use('/', doggo)
+app.use('/', doggo);
+app.use((req, res) => {
+  res.status(404).send('Route not found');
+});
 
 app.listen(config.port, () => {
   console.log(`Listening on port ${config.port}`)
 })
 
 
-export default app;
+module.exports = app;
