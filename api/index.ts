e7ba5a5e9e7dc5711e1fd160b7247d043f24b05d
eric-gebus <ericgebus@gmail.com>
2025-06-23 18:54:21 +0100

    Implemented api/index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,5 @@
-import app from "../src/index";
+import 'dotenv/config';
+
+import app from '../src/index';
 
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -12,11 +12,9 @@ app.use((req, res) => {
   res.status(404).send(`Route ${req.path} not found`);
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running with routes:`);
-  console.log(`POST /new-message`);
-  console.log(`GET  /debug-test`);
-});
-
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+  });
+}
 export default app;
