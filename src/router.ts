7f4ec70946b966c0402044c6cb28e9635982b0e8
eric-gebus <ericgebus@gmail.com>
2025-06-23 19:08:21 +0100

    Test new version

--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,10 @@ import { bark } from "./controller";
 
 const doggo = Router();
 
+doggo.get("/", (req, res) => {
+  res.send("Welcome to DoggoData API!");
+});
+
 doggo.post("/new-message", bark);
 
 doggo.get('/debug-test', (req, res) => {
