35f0618e162bcbb63a0710965d09d4c3b7a9ee7f
eric-gebus <ericgebus@gmail.com>
2025-06-23 21:47:16 +0100

    Remove placeholder route

--- src/router.ts
+++ src/router.ts
@@ -3,14 +3,6 @@ import { bark } from "./controller";
 
 const doggo = Router();
 
-doggo.get("/", (req, res) => {
-  res.send("Welcome to DoggoData API!");
-});
-
 doggo.post("/new-message", bark);
-
-doggo.get('/debug-test', (req, res) => {
-  res.send('ROUTER IS WORKING!');
-});
 
 export default doggo;
