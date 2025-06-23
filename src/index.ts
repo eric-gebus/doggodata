b03bad0c31761397cd56b693d84310199c361086
eric-gebus <ericgebus@gmail.com>
2025-06-23 17:39:39 +0100

    updated test version

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import config from './config/config';
-import cors from 'cors'
+import cors from 'cors';
+import doggo from './router';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors())
+
+app.use('/', doggo)
 
 app.listen(config.port, () => {
   console.log(`Listening on port ${config.port}`)
