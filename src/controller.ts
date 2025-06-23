b7136341f72229ace14842a1133a7855096fa039
eric-gebus <ericgebus@gmail.com>
2025-06-23 20:33:24 +0100

    Optimised api handling

--- src/controller.ts
+++ src/controller.ts
@@ -14,74 +14,45 @@ const allowedMessages = new Set (['woof', 'waff', 'ruff', 'arf', 'wouf', 'waf', 
 
 export async function bark (req: Request, res: Response): Promise<void> {
   try {
-
     if (!process.env.API_KEY) {
       throw new Error('Missing Telegram API key');
     }
 
     const { message } = req.body as { message: Message };
 
-    console.log("Received message:", message);
-
     if (!message) {
-      res.end();
+      res.sendStatus(200);
       return
     }
 
+    res.sendStatus(200);
+
     const messageText = message.text.toLowerCase();
+    let replyText = ''
 
     if (messageText === '/start') {
-      await fetch(`https://api.telegram.org/bot${process.env.API_KEY}/sendMessage`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          chat_id: message.chat.id,
-          text: `Awroo! Welcome to DoggoBot 🐶!\Bark at me with words like "woof", "ruff", or "growl", and I'll tell you a dog fact!`,
-        })
-      });
-    return;
-  }
-
-    if (!allowedMessages.has(messageText)) {
-        await fetch(`https://api.telegram.org/bot${process.env.API_KEY}/sendMessage`,
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            chat_id: message.chat.id,
-            text: `I don't speak hooman, rrruff!`,
-          })
-        });
-      return;
+      replyText = `Awroo! Welcome to DoggoBot 🐶!\Bark at me with words like "woof", "ruff", or "growl", and I'll tell you a dog fact!`
+    } else if (!allowedMessages.has(messageText)) {
+      replyText = `I don't speak hooman, rrruff!`
+    } else {
+      const factRes = await fetch(`https://dogapi.dog/api/v2/facts`);
+      const factData = await factRes.json();
+      replyText = factData.data[0].attributes.body;
     }
 
-    const factRes = await fetch(`https://dogapi.dog/api/v2/facts`);
-    const factData = await factRes.json();
-    const factText = factData.data[0].attributes.body;
 
-    const response = await fetch(`https://api.telegram.org/bot${process.env.API_KEY}/sendMessage`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          chat_id: message.chat.id,
-          text: factText,
-        })
+    await fetch(`https://api.telegram.org/bot${process.env.API_KEY}/sendMessage`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        chat_id: message.chat.id,
+        text: replyText,
       })
+    });
 
-    if (!response.ok) {
-      const errorDetails = await response.text();
-      throw new Error(`Telegram API error: ${response.status} - ${errorDetails}`);
-    };
-
-    res.status(200).send('Message sent successfully');
 
   } catch (err) {
     console.log("Error :", err);
