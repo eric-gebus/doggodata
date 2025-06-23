ec33f0fb2839f31d06cabf5ebe1a33560f23500f
eric-gebus <ericgebus@gmail.com>
2025-06-23 19:26:50 +0100

    Add env var verification

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,9 @@
 import 'dotenv/config';
-
 import app from '../src/index';
+
+if (!process.env.API_KEY) {
+  console.error('FATAL: API_KEY not configured');
+  process.exit(1);
+}
 
 export default app;
